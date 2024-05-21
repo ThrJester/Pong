7d977e2d04b86eb04adae1edc078d3d5a70ee76d
Jude <JudePWhite@gmail.com>
2024-05-21 14:58:07 -0400

    u

--- PongPaddle.java
+++ PongPaddle.java
@@ -1,3 +1,24 @@
 public class PongPaddle {
-    
+    private int top;
+    private int left;
+    private int width;
+    private int height;
+    int getBottomY(){
+        return top - height;
+    }
+    int getLeftX(){
+
+    }
+    int getRightX(){
+
+    }
+    int getTopY(){
+
+    }
+    void moveDown(int d){
+
+    }
+    void moveUp(int d){
+
+    }
 }
--- PongScore.java
+++ PongScore.java
@@ -1,3 +1,9 @@
 public class PongScore {
-    
+    int score;
+    int getScore(){
+        return score;
+    }
+    void scorePoints(int points){
+        score += points;
+    }
 }
